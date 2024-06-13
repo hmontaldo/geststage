b668eb442e5bec49b77356b01a3677e21139b37c
hmontaldo <hmontaldo.ext@simplonformations.onmicrosoft.com>
2024-06-13 21:51:58 +0200

    test certificat

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -5,13 +5,26 @@ $host = 'hen-db-geststage.mysql.database.azure.com';
 $username = 'henry_admin';
 $password = 'Simplon2024@';
 $database = 'bdd_geststages';
+$certificate = "/geststage/include/certificates/DigiCertGlobalRootCA.crt.pem";
 
-function connexionBDD($host, $username, $password, $database)
+// Afficher le chemin du certificat pour vérification
+echo "Chemin du certificat : " . $certificate . "\n";
+
+// Lire et afficher le contenu du certificat
+if (file_exists($certificate)) {
+    $cert_content = file_get_contents($certificate);
+    echo "Contenu du certificat :\n" . $cert_content;
+} else {
+    echo "Le certificat n'existe pas au chemin spécifié.";
+}
+
+function connexionBDD($host, $username, $password, $database, $certificate)
 {
     try
     {
         $db = mysqli_init();
-        mysqli_ssl_set($db, NULL, NULL, "/geststage/include/certificates/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+        // Utiliser la variable $certificate correctement
+        mysqli_ssl_set($db, NULL, NULL, $certificate, NULL, NULL);
         if (!mysqli_real_connect($db, $host, $username, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL))
         {
             throw new Exception('Erreur de connexion : ' . mysqli_connect_error());
@@ -27,7 +40,7 @@ function connexionBDD($host, $username, $password, $database)
 }
 
 // Test de la connexion
-$db = connexionBDD($host, $username, $password, $database);
+$db = connexionBDD($host, $username, $password, $database, $certificate);
 if ($db === false) {
     echo "La connexion à la base de données a échoué.";
 } else {
