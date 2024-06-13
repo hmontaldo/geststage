cbfa5ebbb8965c05f8d5527b095e25ef58d9f04d
hmontaldo <hmontaldo.ext@simplonformations.onmicrosoft.com>
2024-06-13 21:08:58 +0200

    test db

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 // Paramètres de connexion
-$host = 'hen-bantko.mysql.database.azure.com';
+$host = 'hen-db-geststage.mysql.database.azure.com';
 $username = 'henry_admin';
 $password = 'Simplon2024@';
 $database = 'bdd_geststages';
@@ -11,10 +11,10 @@ function connexionBDD()
 {
 	try
 	{
-		$db = mysqli_init();
-		mysqli_ssl_set($db,NULL,NULL, "/geststage/include/certificates/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-		mysqli_real_connect($db, $host, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
-		
+	//	$db = mysqli_init();
+	//	mysqli_ssl_set($db,NULL,NULL, "/geststage/include/certificates/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+	//	mysqli_real_connect($db, $host, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
+	mysqli($host, $username, $password, $database);	
 	}
 	catch(Exception $e)
 	{
