56c9dabd0d4bf2394ab5eea6531bec893bd3eb57
hmontaldo <hmontaldo.ext@simplonformations.onmicrosoft.com>
2024-06-13 21:40:28 +0200

    variables locale

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -1,20 +1,23 @@
 ﻿<?php
 
 // Paramètres de connexion
-$host = 'hen-db-geststage.mysql.database.azure.com';
-$username = 'henry_admin';
-$password = 'Simplon2024@';
-$database = 'bdd_geststages';
+//$host = 'hen-db-geststage.mysql.database.azure.com';
+//$username = 'henry_admin';
+//$password = 'Simplon2024@';
+//$database = 'bdd_geststages';
 
 
 function connexionBDD()
 {
 	try
 	{
+		$host = 'hen-db-geststage.mysql.database.azure.com';
+		$username = 'henry_admin';
+		$password = 'Simplon2024@';
+		$database = 'bdd_geststages';
 		$db = mysqli_init();
 		mysqli_ssl_set($db,NULL,NULL, "/geststage/include/certificates/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 		mysqli_real_connect($db, $host, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
-	//	mysqli($host, $username, $password, $database);
 	}
 	catch(Exception $e)
 	{
