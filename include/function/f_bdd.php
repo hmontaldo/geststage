91211333ec40a0f03336d0bba99cbef4da31064f
hmontaldo <hmontaldo.ext@simplonformations.onmicrosoft.com>
2024-06-13 21:45:19 +0200

    variables accessibles

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -1,30 +1,37 @@
 ﻿<?php
 
 // Paramètres de connexion
-//$host = 'hen-db-geststage.mysql.database.azure.com';
-//$username = 'henry_admin';
-//$password = 'Simplon2024@';
-//$database = 'bdd_geststages';
+$host = 'hen-db-geststage.mysql.database.azure.com';
+$username = 'henry_admin';
+$password = 'Simplon2024@';
+$database = 'bdd_geststages';
 
-
-function connexionBDD()
+function connexionBDD($host, $username, $password, $database)
 {
-	try
-	{
-		$host = 'hen-db-geststage.mysql.database.azure.com';
-		$username = 'henry_admin';
-		$password = 'Simplon2024@';
-		$database = 'bdd_geststages';
-		$db = mysqli_init();
-		mysqli_ssl_set($db,NULL,NULL, "/geststage/include/certificates/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-		mysqli_real_connect($db, $host, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
-	}
-	catch(Exception $e)
-	{
-		$pdo_error = $e->getMessage();
-                return false;
-	}
-    
+    try
+    {
+        $db = mysqli_init();
+        mysqli_ssl_set($db, NULL, NULL, "/geststage/include/certificates/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+        if (!mysqli_real_connect($db, $host, $username, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL))
+        {
+            throw new Exception('Erreur de connexion : ' . mysqli_connect_error());
+        }
+        return $db;
+    }
+    catch(Exception $e)
+    {
+        $pdo_error = $e->getMessage();
+        error_log('Erreur de connexion : ' . $pdo_error);
+        return false;
+    }
+}
+
+// Test de la connexion
+$db = connexionBDD($host, $username, $password, $database);
+if ($db === false) {
+    echo "La connexion à la base de données a échoué.";
+} else {
+    echo "Connexion réussie à la base de données.";
 }
 
 ?>
